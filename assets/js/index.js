574e512d2671f252259807a3ff2c836fda463a68
danielp67 <daniel.67100@hotmail.fr>
2020-06-26 10:21:44 +0200

    sauv day3 10h

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,43 @@
 
 
+// Récupération des blocs
+let mainMenu = document.querySelector("#nav_menu");
+let burgerMenu = document.querySelector("#menu_burger");
 
+/*===============================*/
+/*=== Clic sur le menu burger ===*/
+/*===============================*/
+// Vérifie si l'événement touchstart existe et est le premier déclenché
+let clickedEvent = "click"; // Au clic si "touchstart" n'est pas détecté
+window.addEventListener('touchstart', function detectTouch() {
+	clickedEvent = "touchstart"; // Transforme l'événement en "touchstart"
+	window.removeEventListener('touchstart', detectTouch, false);
+}, false);
+
+// Créé un "toggle class" en Javascrit natif (compatible partout)
+burgerMenu.addEventListener(clickedEvent, function(evt) {
+	console.log(clickedEvent);
+	// Modification du menu burger
+	if(!this.getAttribute("class")) {
+		this.setAttribute("class", "clicked");
+	} else {
+		this.removeAttribute("class");
+	}
+	// Variante avec x.classList (ou DOMTokenList), pas 100% compatible avant IE 11...
+	// burgerMenu.classList.toggle("clicked");
+
+	// Créé l'effet pour le menu slide (compatible partout)
+	if(mainMenu.getAttribute("class") != "visible") {
+		mainMenu.setAttribute("class", "visible");
+	} else {
+		mainMenu.setAttribute("class", "invisible");
+	}
+}, false);
 
 
 //Map integration
 
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+let map = L.map('mapid').setView([47.752727, 7.316758], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 maxZoom: 18,
 id: 'mapbox/streets-v11',
@@ -13,3 +45,46 @@ tileSize: 512,
 zoomOffset: -1,
 accessToken: 'your.mapbox.access.token'
 }).addTo(map);
+
+
+
+
+var overlay = document.getElementById("overlay"),
+  imageBounds = [center, [47.752727, 7.316758]];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+L.imageOverlay(imageUrl, imageBounds).bringToFront();
+
+/*
+let littleton = L.marker([47.752727, 7.316758]).bindPopup('This is Littleton, CO.');
+let cities = L.layerGroup([littleton]);
+
+var grayscale = L.tileLayer(mapboxUrl, {id: 'mapid', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    streets   = L.tileLayer(mapboxUrl, {id: 'mapid', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
+
+var map = L.map('mapid', {
+    center: [47.752727, 7.316758],
+    zoom: 10,
+    layers: [grayscale, cities]
+});
+
+
+var baseMaps = {
+    "Grayscale": grayscale,
+    "Streets": streets
+};
+
+var overlayMaps = {
+    "Cities": cities
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+var baseMaps = {
+    "<span style='color: gray'>Grayscale</span>": grayscale,
+    "Streets": streets
+};
+
+
+*/
